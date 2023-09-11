ba4511e0725df315a2fe75bf26c7a587b7c2320a
laveberry <laveberry7@gmail.com>
2023-09-11 20:31:34 +0900

    JDBC 개발 - 수정, 삭제

--- src/test/java/laveberry/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/laveberry/jdbc/repository/MemberRepositoryV0Test.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,7 +18,7 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberV0", 10000);
+        Member member = new Member("memberV100", 10000);
         repository.save(member);
 
         //findById
@@ -26,5 +27,23 @@ class MemberRepositoryV0Test {
         log.info("member == findMember {}", member == findMember); //false
         log.info("member equals findMember {}", member.equals(findMember)); //true
         assertThat(findMember).isEqualTo(member); //true
+
+        //update : money : 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+//        assertThat(updateMember).isEqualTo(member); //오류
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+//        if (true) {
+//            throw new IllegalStateException("....");
+//        }
+
+        //delete
+        repository.delete(member.getMemberId());
+
+        //삭제 검증 -> NoSuchElementException 뜨는지
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
+
 }

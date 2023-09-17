852c19d2eac58037120082ae42ac8cfdb4ce34a3
laveberry <laveberry7@gmail.com>
2023-09-17 20:22:40 +0900

    예외 포함과 스택 트레이스

--- src/test/java/laveberry/jdbc/exception/basic/UnCheckedAppTest.java
+++ src/test/java/laveberry/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,11 +1,13 @@
 package laveberry.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.*;
 
+@Slf4j
 public class UnCheckedAppTest {
 
     @Test
@@ -13,6 +15,17 @@ public class UnCheckedAppTest {
         Controller controller = new Controller();
         assertThatThrownBy(() -> controller.request())
                 .isInstanceOf(RuntimeSQLException.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            // 로그 출력시 마지막 파라미터에 예외를 넣어주면, 로그에 스택 트레이스를 출력할 수 있다. (Caused by:)
+            log.info("ex", e);
+        }
     }
 
     static class Controller {
